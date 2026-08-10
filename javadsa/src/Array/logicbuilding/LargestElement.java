355dfbb26b69caff4c434e0f854bffdaef1925bc
pranjal tyagi <tyagipranjal272@gmail.com>
2026-08-10 09:49:56 +0530

    EG

--- javadsa/src/Array/logicbuilding/LargestElement.java
+++ javadsa/src/Array/logicbuilding/LargestElement.java
@@ -1,6 +1,6 @@
 package Array.logicbuilding;
 
-public class LargestElement {
+public class LargestElement{
     public static int maxvalue(int arr[]){
         int max = Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
@@ -13,6 +13,6 @@ public class LargestElement {
     }
     public static void main(String[] args){
         int arr[] = {1,2,3,4,5,6,7};
-                System.out.println("This is the Maximum Number in an Array: " + maxvalue(arr));
+        System.out.println("This is the Maximum Number in an Array: " + maxvalue(arr));
     }
 }
---
+++ javadsa/src/Array/logicbuilding/LearningArrayList.java
@@ -0,0 +1,17 @@
+package Array.logicbuilding;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class LearningArrayList {
+    public static void main(String[] args){
+        ArrayList<Integer>l1=new ArrayList<>();
+        l1.add(0,5);
+        l1.add(1,6);
+        l1.add(2,7);
+        l1.add(3,20);
+
+        System.out.println(l1);
+
+    }
+}
